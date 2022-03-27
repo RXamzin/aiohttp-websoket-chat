0c72860ac48872b180a5f11f10cf702ed30c0a15
RXamzin <raulhamzin@gmail.com>
2022-03-27 17:01:38 +0300

    Base layout

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
+import os
 from app.views.view import index
+from app.config import BASE_DIR
 
 def set_up_routes(app):
     app.router.add_get('/', index)
+    app.router.add_static('/static', path=os.path.join(BASE_DIR, 'static'), name='static', append_version=True)
