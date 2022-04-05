fc63c56ad1bd060019c587b875564d35d8f1a5ac
RXamzin <raulhamzin@gmail.com>
2022-04-05 20:25:43 +0300

    v1

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,26 +1,38 @@
+let ws
+
 $(document).on('click', '.join_btn', (e) => {
-    let name = $('.name_input').val().trim()
+    let name = $('.name_input').val().trim();
     if (name) {
-        $('.name_input').prop('disabled', true)
-        $(e.target).hide()
-        $('.exit_btn').show()
-        var ws = create_and_connect_to_websocket(name) // Set ws global
+        $('.name_input').prop('disabled', true);
+        $(e.target).hide();
+        $('.exit_btn').show();
+        $('.send_btn').show();
+        $('.msg_input').show();
+        ws = create_and_connect_to_websocket(name); // Set ws global
     }
 })
 
 $(document).on('click', '.exit_btn', (e) => {
-    $('.name_input').prop('disabled', false)
-    $(e.target).hide()
-    $('.join_btn').show()
+    $('.name_input').prop('disabled', false);
+    $(e.target).hide();
+    $('.send_btn').hide();
+    $('.msg_input').val('');
+    $('.msg_input').hide();
+    $('.join_btn').show();
+
+    msg = {};
+    msg['TYPE'] = 'EXIT';
+    msg['USERNAME'] = $('.name_input').val().trim();
+    ws.send(JSON.stringify(msg));
 })
 
 function create_and_connect_to_websocket(name) {
-    let ws = new WebSocket('ws://' + window.location.host + '/ws')
-    ws.onopen = function() {
-        msg = {}
-        msg['TYPE'] = 'CONNECTION'
-        msg['USERNAME'] = name
-        ws.send(JSON.stringify(msg))
+    ws = new WebSocket('ws://' + window.location.host + '/ws');
+    ws.onopen = function () {
+        msg = {};
+        msg['TYPE'] = 'CONNECTION';
+        msg['USERNAME'] = name;
+        ws.send(JSON.stringify(msg));
     }
     ws.onmessage = function (event) {
         let msg_data = JSON.parse(event.data);
@@ -30,9 +42,27 @@ function create_and_connect_to_websocket(name) {
         html += `<strong>${msg_data['USERNAME']}:</strong>`;
         html += `<p>${msg_data['MESSAGE']}</p>`;
         html += '</div>';
-        $('.chat_div').append(html)
-      }
-    return ws
+        $('.chat_div').append(html);
+    }
+    return ws;
 }
 
-function recieve_msg() {}
+$(document).on('click', '.send_btn', (e) => {
+    let text = $('.msg_input').val().trim();
+
+    if (text) {
+        msg = {};
+        msg['TYPE'] = 'MESSAGE';
+        msg['USERNAME'] = $('.name_input').val().trim();
+        msg['MESSAGE'] = text
+        ws.send(JSON.stringify(msg));
+        $('.msg_input').val('')
+
+        let html = '';
+        html += '<div class="message">';
+        html += `<strong>${msg['USERNAME']}:</strong>`;
+        html += `<p>${msg['MESSAGE']}</p>`;
+        html += '</div>';
+        $('.chat_div').append(html);
+    }
+})
