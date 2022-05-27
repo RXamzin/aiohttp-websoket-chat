c87d744ba90462e8d8c953909ccb67cf3250cdca
RXamzin <raulhamzin@gmail.com>
2022-05-27 23:00:34 +0300

    Update requirements and add configs

--- app/config.py
+++ app/config.py
@@ -1,3 +1,11 @@
 import os
+import dotenv
+import secrets
+
+dotenv.load_dotenv()
 
 BASE_DIR = os.path.dirname(__file__)
+
+class Config():
+
+    SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex(20))
